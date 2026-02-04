d7f5d1b14bafa38ccadd88add361dd1d73689c39
Raggini Marco <marco.raggini2@studio.unibo.it>
2026-02-04 14:17:45 +0100

    chore: add mlflow tracking

--- GoT/ollama_llm.py
+++ GoT/ollama_llm.py
@@ -1,32 +1,30 @@
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import SystemMessage, HumanMessage
 from langchain.agents import create_agent
+import mlflow
 
 from GoT.tools.math_tool import summing, minus, square_root, daytime
 
+mlflow.set_experiment("marcoraggini-experiment")
+mlflow.openai.autolog()
+mlflow.langchain.autolog()
+
 class OllamaLLM:
     def __init__(self):
-        self.ollamaLLM = ChatOpenAI(
-            base_url="http://localhost:11434/v1",
-            api_key="dummy",
-            model="ministral-3:8b",
-        )
+        with mlflow.set_active_model(name="ollama-agent-ministral-3"):
+            self.ollamaLLM = ChatOpenAI(
+                base_url="http://localhost:11434/v1",
+                api_key="dummy",
+                model="ministral-3:8b",
+            )
 
-        self.system_prompt = SystemMessage("""
-            You are a helpful assistant able to address general problems. 
-            ...
-        """)
+            self.system_prompt = SystemMessage("""
+                You are a helpful assistant able to address general problems. 
+                ...
+            """)
 
-        self.agent = create_agent(
-            model=self.ollamaLLM,
-            tools=[summing, minus, square_root, daytime],
-            system_prompt=self.system_prompt,
-        )
-
-        self.messages = [
-            HumanMessage("Solve the equation x^3 - 2x - 5 = 0 numerically."),
-        ]
-
-    def invoke(self): 
-        return self.agent.invoke({"messages": self.messages})
-    
+            self.agent = create_agent(
+                model=self.ollamaLLM,
+                tools=[summing, minus, square_root, daytime],
+                system_prompt=self.system_prompt,
+            )
