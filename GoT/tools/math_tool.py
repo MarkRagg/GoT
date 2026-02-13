efcc206d477b3ca5a07f47ed25042a14083eaf7f
Raggini Marco <marco.raggini2@studio.unibo.it>
2026-02-13 15:04:15 +0100

    chore: add description on tools

--- GoT/tools/math_tool.py
+++ GoT/tools/math_tool.py
@@ -4,35 +4,67 @@ from langchain.tools import tool
 
 @tool
 def summing(x: int, y: int) -> int:
-    """sum of two integer"""
+    """sum of two integer
+    
+    Arguments:
+        x(int): first number
+        y(int): second number
+    """
     return x + y
 
 
 @tool
 def sum_three(x: int, y: int, z: int):
-    """sum of three integer"""
+    """sum of three integer
+    
+    Arguments:
+        x(int): first number
+        y(int): second number
+        z(int): third number
+    """
     return x + y + z
 
 
 @tool
 def sum_four(x, y, z, a):
-    """sum of four integer"""
+    """sum of four integer
+    
+    Arguments:
+        x(int): first number
+        y(int): second number
+        z(int): third number
+        a(int): fourth number
+    """
     return x + y + z + a
 
 
 @tool
 def minus(x: int, y: int) -> int:
-    """minus of two integer"""
+    """minus of two integer
+    
+    Arguments:
+        x(int): first number
+        y(int): second number
+    """
     return x - y
 
 
 @tool
 def multiply(x: int, y: int) -> int:
-    """multiply of two integer"""
+    """multiply of two integer
+    
+    Arguments:
+        x(int): first number
+        y(int): second number
+    """
     return x * y
 
 
 @tool
-def square_root(x):
-    """Calculate the square root of x"""
+def square_root(x: int) -> float:
+    """Calculate the square root of x
+    
+    Arguments:
+        x(int): a non-negative number
+    """
     return sqrt(x)
