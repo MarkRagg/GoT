64938ea856c42c03fca9960b5e1a043af1ca3ac1
Raggini Marco <marco.raggini2@studio.unibo.it>
2026-02-18 16:56:00 +0100

    feat: introduce lm eval test benchmark

--- GoT/__init__.py
+++ GoT/__init__.py
@@ -3,13 +3,29 @@ import logging
 
 from lm_eval import evaluator, tasks
 from GoT.model.graph_model import call_graph
-from GoT.model.lm_wrapper import LangGraphLMWrapper
+from GoT.model.lm_wrapper import LangGraphLMWrapper, OllamaTestLMWrapper
 
 # logging.basicConfig(level=logging.DEBUG)
 # logger = logging.getLogger("GoT")
 
 
-def lm_eval_benchmark():
+def lm_eval_test_benchmark():
+    task_list = ["gsm8k"]
+    test_lm = OllamaTestLMWrapper()
+    task_dict = tasks.get_task_dict(task_list)
+
+    results = evaluator.evaluate(
+        lm=test_lm,
+        task_dict=task_dict,
+        limit=10,  # Limit the number of samples
+        log_samples=True,
+    )
+
+    # Save results to a JSON file
+    with open("ollama_test_benchmark_results.json", "w") as f:
+        json.dump(results, f, indent=2)
+
+def lm_eval_graph_benchmark():
     task_list = ["gsm8k"]
     lm = LangGraphLMWrapper()
     task_dict = tasks.get_task_dict(task_list)
@@ -22,7 +38,7 @@ def lm_eval_benchmark():
     )
 
     # Save results to a JSON file
-    with open("graph_benchmark_results.json", "w") as f:
+    with open("ollama_test_benchmark_results.json", "w") as f:
         json.dump(results, f, indent=2)
 
 
@@ -32,7 +48,7 @@ def custom_test():
 
 def main():
     # It could be changed with custom_test() to test a custom problem instead of the benchmark
-    lm_eval_benchmark()
+    lm_eval_graph_benchmark()
 
 
 # let this be the last line of this file
