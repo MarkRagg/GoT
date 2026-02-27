2869b4d2e3b9aa6de3d1bbd942b075e08d66cdc8
Raggini Marco <marco.raggini2@studio.unibo.it>
2026-02-27 09:56:47 +0100

    chore: fix for mypy

--- GoT/__init__.py
+++ GoT/__init__.py
@@ -1,13 +1,19 @@
 import json
 import logging
+from dotenv import load_dotenv
 
 from lm_eval import evaluator, tasks
 from GoT.model.graph_model import call_graph
-from GoT.model.lm_wrapper import LangGraphLMWrapper, OllamaTestLMWrapper
+from GoT.model.lm_wrapper import (
+    LangGraphLMWrapper,
+    OllamaTestLMWrapper,
+)
 from GoT.model.utils.utils import print_benchmark_result
 
-# logging.basicConfig(level=logging.DEBUG)
-# logger = logging.getLogger("GoT")
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger("GoT")
+
+load_dotenv()
 
 
 def lm_eval_test_benchmark():
@@ -18,7 +24,7 @@ def lm_eval_test_benchmark():
     results = evaluator.evaluate(
         lm=test_lm,
         task_dict=task_dict,
-        limit=10,  # Limit the number of samples
+        limit=20,  # Limit the number of samples
         log_samples=True,
     )
 
@@ -28,33 +34,37 @@ def lm_eval_test_benchmark():
 
     print_benchmark_result(results)
 
+
 def lm_eval_graph_benchmark():
-    task_list = ["gsm8k"]
+    task_name = "gsm8k"
+    task_list = [task_name]
     lm = LangGraphLMWrapper()
     task_dict = tasks.get_task_dict(task_list)
 
     results = evaluator.evaluate(
         lm=lm,
         task_dict=task_dict,
-        limit=10,  # Limit the number of samples
+        limit=5,  # Limit the number of samples
         log_samples=True,
     )
 
     # Save results to a JSON file
-    with open("ollama_test_benchmark_results.json", "w") as f:
+    with open("ollama_graph_benchmark_results.json", "w") as f:
         json.dump(results, f, indent=2)
 
-    print_benchmark_result(results)
+    print_benchmark_result(results, task_name)
 
 
 def custom_test():
-    call_graph("What is 4726621 + 2 * 392 - 3432?")
+    call_graph(
+        "Josh decides to try flipping a house. He buys a house for $80,000 and then puts in $50,000 in repairs. This increased the value of the house by 150%. How much profit did he make?"
+    )
 
 
 def main():
     # It could be changed with custom_test() to test a custom problem instead of the benchmark
-    lm_eval_test_benchmark()
+    lm_eval_graph_benchmark()
 
 
 # let this be the last line of this file
-# logger.info("GoT loaded")
+logger.info("GoT loaded")
