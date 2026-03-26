3b492e81d8085c1957117bed61f3cb489e590b71
Raggini Marco <marco.raggini2@studio.unibo.it>
2026-03-26 14:34:55 +0100

    chore: use gpqa diamond as benchmark

--- GoT/__init__.py
+++ GoT/__init__.py
@@ -5,10 +5,10 @@ from dotenv import load_dotenv
 from lm_eval import evaluator, tasks
 from GoT.model.graph_model import call_graph
 from GoT.model.lm_wrapper import (
-    OllamaTestLMWrapper,
     LangGraphBigBenchWrapper,
+    TestBigBenchWrapper
 )
-from GoT.model.utils.utils import print_benchmark_result
+from GoT.model.utils.utils import print_benchmark_result, print_benchmark_result_loglikehood
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger("GoT")
@@ -19,49 +19,50 @@ load_dotenv()
 
 
 def lm_eval_test_benchmark():
-    task_name = "gsm8k"
+    task_name = "gpqa_diamond_zeroshot"
     task_list = [task_name]
-    test_lm = OllamaTestLMWrapper()
+    test_lm = TestBigBenchWrapper()
     task_dict = tasks.get_task_dict(task_list)
 
     results = evaluator.evaluate(
         lm=test_lm,
         task_dict=task_dict,
-        limit=20,  # Limit the number of samples
+        limit=2,  # Limit the number of samples
         log_samples=True,
+        # samples={task_name: [20, 25, 100]},
     )
 
     # Save results to a JSON file
-    with open("ollama_test_benchmark_results.json", "w") as f:
-        json.dump(results, f, indent=2)
+    with open("test_benchmark_results.json", "w") as f:
+        json.dump(results["samples"], f, indent=2)
 
-    print_benchmark_result(results, task_name, filter="none")
+    print_benchmark_result(results, task_name, filter="strict-match")
 
 
 def lm_eval_graph_benchmark():
     # hendrycks_math_geometry
-    task_name = "gsm8k"
+    task_name = "gpqa_diamond_zeroshot"
     task_list = [task_name]
     lm = LangGraphBigBenchWrapper()
     task_dict = tasks.get_task_dict(task_list)
 
     results = evaluator.evaluate(
         lm=lm,
-        # limit=10,
+        # limit=1,
         task_dict=task_dict,
-        samples={task_name: [5, 10, 20, 50, 100, 150, 127, 152]},
+        samples={task_name: [20, 25]},
         log_samples=True,
     )
 
     # Save results to a JSON file
-    with open("ollama_graph_benchmark_results.json", "w") as f:
-        json.dump(results, f, indent=2)
+    with open("graph_benchmark_results.json", "w") as f:
+        json.dump(results, f, indent=2, default=str)
 
-    print_benchmark_result(results, task_name, filter="flexible")
+    print_benchmark_result_loglikehood(results, task_name, filter_val="none")
 
 
 def custom_test():
-    call_graph("∫x2⋅ex2dx")
+    call_graph("Solve this integral ∫x2⋅ex2dx")
 
 
 def main():
