c560aa1a0699c6e9984c768f8ee398d8c519c96c
Raggini Marco <marco.raggini2@studio.unibo.it>
2026-03-31 17:28:35 +0200

    feat: add parser in order to launch from terminal

---
+++ GoT/model/utils/parse_args.py
@@ -0,0 +1,28 @@
+import argparse
+import sys
+
+from GoT.model.utils.hf_formatter import use_gpqa, use_gsm8k
+
+
+
+def defining_and_parse_args():
+    parser = argparse.ArgumentParser(description="Run the GoT model on a benchmark or a custom problem.")
+    parser.add_argument("--benchmark", required=True, type=str, choices=["gsm8k", "gpqa"], help="The benchmark to run the model on.")
+    parser.add_argument("--mode", required=True, type=str, choices=["graph", "standard"], help="Whether to run the standard model or the graph model.")
+    parser.add_argument("--max_run", type=int, default=1, help="The maximum number of runs for the benchmark.")
+
+    if len(sys.argv) == 1:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
+    
+    args = parser.parse_args()
+    return args
+
+def call_benchmark(args):
+    mode = args.mode if args.mode else "standard"
+    test = mode == "standard"
+    max_run = args.max_run
+    if args.benchmark == "gsm8k":
+        use_gsm8k(max_run=max_run, test=test, model_name=mode)
+    elif args.benchmark == "gpqa":
+        use_gpqa(max_run=max_run, test=test, model_name=mode)
