47ef8b4a50dd5072bf7b145b35f6ed29e566caa1
Raggini Marco <marco.raggini2@studio.unibo.it>
2026-05-14 14:46:39 +0200

    feat: add custom runs on cli args

--- GoT/cli/parse_args.py
+++ GoT/cli/parse_args.py
@@ -7,6 +7,7 @@ from GoT.experiments.hf_formatter import (
     use_gsm8k,
     use_hendrycks_math,
 )
+from GoT.experiments.runner_custom import custom_test
 
 
 def defining_and_parse_args():
@@ -17,7 +18,7 @@ def defining_and_parse_args():
         "--benchmark",
         required=True,
         type=str,
-        choices=["gsm8k", "gpqa", "hendrycks_math", "gaia"],
+        choices=["gsm8k", "gpqa", "hendrycks_math", "gaia", "custom"],
         help="The benchmark to run the model on.",
     )
     parser.add_argument(
@@ -26,6 +27,12 @@ def defining_and_parse_args():
         type=str,
         choices=["graph", "standard"],
         help="Whether to run the standard model or the graph model.",
+    )
+    parser.add_argument(
+        "--prompt",
+        type=str,
+        default="",
+        help="Insert a prompt during a custom run."
     )
     parser.add_argument(
         "--max_run",
@@ -69,3 +76,5 @@ def call_benchmark(args):
         use_hendrycks_math(max_run=max_run, test=test, model_name=mode, type=args.type)
     elif args.benchmark == "gaia":
         use_gaia(max_run=max_run, test=test, model_name=mode)
+    elif args.benchmark == "custom" and args.prompt != "":
+        custom_test(args.prompt, test)
