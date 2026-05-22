6bfc5c437e7ff4087a2f6e1bdde2825923169786
Raggini Marco <marco.raggini2@studio.unibo.it>
2026-05-22 14:59:29 +0200

    chore: uncomment pypi config

--- release.config.mjs
+++ release.config.mjs
@@ -1,25 +1,25 @@
 let dryRun = (process.env.RELEASE_DRY_RUN || "false").toLowerCase() === "true";
-// let testPypi = (process.env.RELEASE_TEST_PYPI || "false").toLowerCase() === "true";
-// const pypiToken = process.env.PYPI_TOKEN;
+let testPypi = (process.env.RELEASE_TEST_PYPI || "false").toLowerCase() === "true";
+const pypiToken = process.env.PYPI_TOKEN;
 
 let prepareCmd = "poetry version -- \${nextRelease.version}" + ` && poetry config pypi-token.pypi ${pypiToken}`;
-// let publishCmd = `poetry publish --build`;
+let publishCmd = `poetry publish --build`;
 
-// if (testPypi) {
-//     publishCmd += ` --repository testpypi`;
-//     prepareCmd = prepareCmd.replace("pypi-token.pypi", "pypi-token.testpypi");
-// }
+if (testPypi) {
+    publishCmd += ` --repository testpypi`;
+    prepareCmd = prepareCmd.replace("pypi-token.pypi", "pypi-token.testpypi");
+}
 
-// if (dryRun) {
-//     publishCmd += " --dry-run";
-// }
+if (dryRun) {
+    publishCmd += " --dry-run";
+}
 
 import config from 'semantic-release-preconfigured-conventional-commits' with {type: 'json'};
 
 config.plugins.push(
     ["@semantic-release/exec", {
         "prepareCmd" : prepareCmd,
-        // "publishCmd": publishCmd,
+        "publishCmd": publishCmd,
     }]
 )
 
